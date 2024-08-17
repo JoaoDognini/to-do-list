a076e277a01c6dbe59f49e7bf7136abd8021e74e
João Otávio Dognini Azevedo <joaootavioazevedo@gmail.com>
2024-08-17 11:36:02 -0300

    Modal e Ações
    - Criação de Modal reutilizável
    - Configuração do Modal para as ações de deletar e concluir, e também para adicionar e editar tarefa
    - Estilização básica, não finalizada
    - Alterado configuração padrão de tema claro/escuro

--- to-do-list/src/App.vue
+++ to-do-list/src/App.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue';
+import Modal from './components/Modal.vue';
 
 class Tarefa {
 	id: number = 0;
 	tarefa: string = '';
 }
 
-const lista = reactive<Tarefa[]>([]);
-const listaConcluida = reactive<Tarefa[]>([]);
+const showModal = ref(false);
+const ehModalConfirmacao = ref(false);
+const tituloModal = ref('');
+const deletar = ref(false)
+const novaTarefa = ref('');
+const tarefaSelecionada = ref<Tarefa>();
+let lista = reactive<Tarefa[]>([]);
+let listaConcluida = reactive<Tarefa[]>([]);
 
 lista.push(
 	{
@@ -29,8 +36,7 @@ lista.push(
 	{
 		id: 5,
 		tarefa: 'Teste tarefa 5'
-	},
-)
+	});
 
 listaConcluida.push(
 	{
@@ -40,24 +46,51 @@ listaConcluida.push(
 	{
 		id: 2,
 		tarefa: 'Teste tarefa concluída 2'
-	})
-
+	});
 
 function editarTarefa(tarefa: Tarefa) {
-
+	abrirModal(false, 'Editar tarefa', tarefa);
+	novaTarefa.value = tarefa.tarefa;
 };
 
 function deletarTarefa(idTarefa: number) {
 	const index = lista.findIndex(x => x.id === idTarefa);
-	lista.splice(index, 1)
+	lista.splice(index, 1);
 };
 
 function concluirTarefa(tarefa: Tarefa) {
-	deletarTarefa(tarefa.id)
-	listaConcluida.push({ id: tarefa.id, tarefa: tarefa.tarefa })
+	deletarTarefa(tarefa.id);
+	listaConcluida.push(tarefa);
 };
 
-function adicionarTarefa() { }
+function confirmar() {
+	deletar.value ? deletarTarefa(tarefaSelecionada.value!.id) : concluirTarefa(tarefaSelecionada.value!);
+
+	fecharModal();
+}
+
+function cadastrarOuEditarTarefa() {
+	if (tarefaSelecionada.value) {
+		const indexAtualizar = lista.findIndex(item => item.id === tarefaSelecionada.value?.id);
+		lista[indexAtualizar] = { ...lista[indexAtualizar], tarefa: novaTarefa.value };
+	} else {
+		const id = lista.length + 1;
+		lista.push({ id: id, tarefa: novaTarefa.value });
+	}
+	novaTarefa.value = '';
+}
+
+const fecharModal = () => {
+	showModal.value = false
+}
+
+const abrirModal = (confirmacao: boolean, titulo: string, tarefa?: Tarefa, ehDeletar: boolean = false) => {
+	showModal.value = true;
+	ehModalConfirmacao.value = confirmacao;
+	tituloModal.value = titulo;
+	tarefaSelecionada.value = tarefa;
+	deletar.value = ehDeletar;
+}
 
 </script>
 
@@ -70,7 +103,7 @@ function adicionarTarefa() { }
 		<section class="tarefas">
 			<div class="tarefas_topo">
 				<h1>Tarefas</h1>
-				<img src="./assets/icons/adicionar.svg" alt="" @click="adicionarTarefa()">
+				<img src="./assets/icons/adicionar.svg" alt="" @click="abrirModal(false, 'Adicionar tarefa')">
 			</div>
 			<div class="tarefas_conteudo">
 				<span v-if="!lista.length">Nenhuma tarefa.</span>
@@ -78,8 +111,8 @@ function adicionarTarefa() { }
 					<li v-for="tarefa in lista">
 						<div class="icones">
 							<img src="./assets/icons/edit.svg" @click="editarTarefa(tarefa)" />
-							<img src="./assets/icons/delete.svg" @click="deletarTarefa(tarefa.id)" />
-							<img src="./assets/icons/concluir.svg" @click="concluirTarefa(tarefa)" />
+							<img src="./assets/icons/delete.svg" @click="abrirModal(true, '', tarefa, true)" />
+							<img src="./assets/icons/concluir.svg" @click="abrirModal(true, '', tarefa)" />
 						</div>
 						<p>{{ tarefa.tarefa }}</p>
 					</li>
@@ -101,6 +134,16 @@ function adicionarTarefa() { }
 				</ul>
 			</div>
 		</section>
+
+		<Modal :is-open="showModal" :titulo="tituloModal" :eh-modal-confirmacao="ehModalConfirmacao"
+			@modal-close="fecharModal()" @confirmar="confirmar()">
+			<template #content>
+				<form @submit.prevent="cadastrarOuEditarTarefa()">
+					<input type="text" name="tarefa" placeholder="Digite a tarefa" v-model="novaTarefa" required>
+					<button type="submit">Adicionar</button>
+				</form>
+			</template>
+		</Modal>
 	</main>
 </template>
 
@@ -170,5 +213,34 @@ main section .tarefas_conteudo {
 .tarefas_concluidas span {
 	padding: 1rem;
 	display: block;
+}
+
+form {
+	display: flex;
+	flex-direction: column;
+}
+
+form input {
+	width: 100%;
+	padding: 1rem;
+	font-size: 1rem;
+	border-radius: 5px;
+	border: 1px solid var(--vt-c-black-mute);
+	margin-bottom: 1rem;
+}
+
+form button {
+	padding: 1rem;
+	border: none;
+	background-color: #125FFF;
+	color: #FFF;
+	font-weight: 600;
+	cursor: pointer;
+	align-self: end;
+	width: max-content;
+}
+
+input:focus {
+	outline: none;
 }
 </style>
